7ed8c1eac62f264a038da022b650fec78fbe54d3
KayKay <174835284+Bananabread58@users.noreply.github.com>
2026-06-18 13:35:29 -0700

    Made tip calculator

--- practice.py
+++ practice.py
@@ -1,9 +1,23 @@
 
+print("Welcome to the tip calculator!")
+bill = int(input("What was the total bill? "))
+tip = int(input("What is the percentage of tip you would like to give to ? 10 12 or 15? "))
+split = int(input("How many people to split the bill? "))
+
+
+tip_total= (float(bill * tip / 100))
+total_bill = (bill + tip_total)
+split_amount = (total_bill / split)
+the_total = (split_amount)
+
+# print((the_total), "is your total amount")
+
+print(f"{split_amount:.2f} is your total amount")
 
 
 
-print("Welcome to the band name generator")
-city = input("Whats the name of your city?")
-pet = input("Whats the name of your pet?")
+# f":.2f" {}
 
-print("your band name is ", city + " " + pet)
+# sally_mae = it(input("How much is the tip? "))
+
+# print(sally_mae)
