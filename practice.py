a048adb42640104df2e941cbd24508e0eebb4135
KayKay <174835284+Bananabread58@users.noreply.github.com>
2026-07-02 14:01:10 -0700

    Pizza place delivery, added sizes prices and bill

--- practice.py
+++ practice.py
@@ -1,23 +1,58 @@
-
-print("Welcome to the tip calculator!")
-bill = int(input("What was the total bill? "))
-tip = int(input("What is the percentage of tip you would like to give to ? 10 12 or 15? "))
-split = int(input("How many people to split the bill? "))
-
-
-tip_total= (float(bill * tip / 100))
-total_bill = (bill + tip_total)
-split_amount = (total_bill / split)
-the_total = (split_amount)
-
-# print((the_total), "is your total amount")
-
-print(f"{split_amount:.2f} is your total amount")
+small = 15
+medium = 20
+large = 25
+bill = 0 
+pepperonismall = 2
+pepperoniregular = 3
+extracheese = 1
 
 
+print("Welcome to tupac's pizza")
+order = (input("Would you like a pizza? y/n "))
+tupac_order = print(order)
 
-# f":.2f" {}
+if order == "y":
+    size = (input("What size would you like? small, medium, or large?  "))
+    if size == "small": 
+        bill = small
+    elif size == "medium":
+       bill = medium 
+    elif size == "large":
+        bill = large
+        print(bill)
+    toppings = (input("Would you like pepperoni? "))
+    if toppings == "y" and size == "small":
+        bill = bill + pepperonismall
+    elif toppings == "y" and size == "medium":
+        bill = bill + pepperoniregular
+    elif toppings == "y" and size == "large":
+        bill = bill + pepperoniregular
+    else: 
+       print("No toppings")
+    toppings2 = (input("Would you like extra cheese with that "))
+    if toppings2 == "y": 
+        bill = bill + extracheese    
+    else: 
+        print("No extra cheese")
 
-# sally_mae = it(input("How much is the tip? "))
 
-# print(sally_mae)
+else:
+  print("You didn't order anything. Please Order Something OR GET OUT!")
+       
+
+print(f"Your total is {bill} ")
+
+     
+   
+    
+
+
+
+
+
+
+
+
+
+
+
