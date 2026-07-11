2fa54961a20f642b661cc200acf760c23100cde7
KayKay <174835284+Bananabread58@users.noreply.github.com>
2026-07-10 17:16:01 -0700

    Created game around 30ish questions

--- practice.py
+++ practice.py
@@ -1,50 +1,145 @@
-small = 15
-medium = 20
-large = 25
-bill = 0 
-pepperonismall = 2
-pepperoniregular = 3
-extracheese = 1
+left = "left"
+right = "right"
+gameover = "gameover"
 
 
-print("Welcome to tupac's pizza")
-order = (input("Would you like a pizza? y/n "))
-tupac_order = print(order)
-
-if order == "y":
-    size = (input("What size would you like? small, medium, or large?  "))
-    if size == "small": 
-        bill = small
-    elif size == "medium":
-       bill = medium 
-    elif size == "large":
-        bill = large
-        print(bill)
-    toppings = (input("Would you like pepperoni? "))
-    if toppings == "y" and size == "small":
-        bill = bill + pepperonismall
-    elif toppings == "y" and size == "medium":
-        bill = bill + pepperoniregular
-    elif toppings == "y" and size == "large":
-        bill = bill + pepperoniregular
-    else: 
-       print("No toppings")
-    toppings2 = (input("Would you like extra cheese with that "))
-    if toppings2 == "y": 
-        bill = bill + extracheese    
-    else: 
-        print("No extra cheese")
 
 
+
+print("Of mice and men game practice")
+print("It was a warm evening with a little wind that moves amongst the leaves, there a river for you an your friend George to take a drink")
+choice1 = input(("The water looked scummy and was painted a muddy grey, would you like to take a sip ? y/n "))
+if choice1 == "n":
+    print("You died from dehydration ")
+elif choice1 == "y":
+    choice2 = input(("Though the water was unappetizing, it quenched your thirst. You've got a can of beans to share with George(y), but you can go to the ranch for food(n) will you stay? "))
+    if choice2 == "n": 
+        print("You got hit by a car")
+    elif choice2 == "y": 
+        choice3 = input(("You decide to stay with your friend and share the can of beans, there no ketchup, will you still eat those beans? y/n "))
+        if choice3 == "n": 
+         gameover = print("You die of starvation")
+        elif choice3 == "y":
+          choice4 = input(("You tough it out with the ketchup, you see a mouse and pick it up, it dies. Your friend scolds you for trying to keep it, will you let it go? y/n"))
+          if choice4 == "n":
+             print("Your friend leaves you to cool off, you decide to venture somewhere else and drown in the river")
+          elif choice4 == "y":
+             choice5 = print(("You let the mouse go, and you head to bed awaiting new oppertunites for tomorrow"))
+             choice5 = print("CHAPTER 2: LENNIE")  
+             choice5 = input(("You see the bunk house in the distance, thats where your new job will be located, do you want to step inside? y/n"))
+             if choice5 == "n": 
+                print("You can't afford the cost of living, you perish from lack of finances to survive")
+             elif choice5 == "y":
+                choice6 = input((" You head inside for work and notice eight bunks lying against the walls, you see a loose screw coming out of one, do you fix it? y/n"))
+                if choice6 == "y":
+                   print("The beds toppled over and crushed you to death")
+                elif choice6 == "n": 
+                   choice7 = input(("The beds didn't seem to sturdy so you kept your distance. Your friend tells you not to say much during the interview, will you listen? y/n"))
+                   if choice7 == "n":
+                      print("You didn't get the job, you lose")
+                   elif choice7 == "y":
+                      choice8 = input(("The interview is going well but you notice George lies about you too being cousins. Do you correct him? y/n "))
+                      if choice8 == "y":
+                         print("You didn't get the job, you lose")
+                      elif choice8 == "n":
+                         choice9 = input(("You decided to keep quiet and made the job, a man named curly; the bosses son comes pretty rude to you, will you call him out? y/n"))
+                         if choice9 == "y":
+                            print("You lost your job from being rude to the bosses son even when you were standing up for yourself, you lose")
+                         elif choice9 == "n":
+                            choice10 = input(("You once again decide to keep quiet for the best of it, you see curlys wife pass by and curly asks where she went, do you respond? y/n"))
+                            if choice10 == "y":
+                               print("He gets mad at you and george stands in fear while watching him go at you, you lose your job")
+                            elif choice10 == "n":
+                               choice11 = print("George responds by saying no and curly gets irriated at him.")
+                               choice11 = print("CHAPTER 3: Lennie")
+                            choice11 = input(("You got the old mans dog in your bunk, george asks you to to give it to him, do you y/n?"))
+                            if choice11 == "n":
+                             print("You kill it on accident and the old man never forgives you, you lose")
+                            elif choice11 == "y":
+                                choice12 = input(("You give back the dog and the others talk on how the dog stinks, they say they'll kill him tomorrow, do you talk them out of it? y/n"))
+                                if choice12 == "y":
+                                 print("You get into a fight with the others and lose your job")
+                                elif choice12 == "n":
+                                   choice13 = input(("You hold your toungue, curly is still upset no one tells him about his wife he slashes you with his hand, do you fight back? y/n"))
+                                   if choice13 == "n":
+                                    print("He beats you to death")
+                                   elif choice13 == "y":
+                                      choice14 = input(("You fight back against him, and nearly kill him, george yells to let go? Do you? y/n"))
+                                      if choice14 == "n":
+                                       print("He dies, you lose your job and go to jail")
+                                      elif choice14 == "y":
+                                         choice15 = input(("You let go of him, are you sure you don't want to finish him? y/n"))
+                                         if choice14 == "n":
+                                          print("He dies, you lose your job and go to jail")
+                                         elif choice15 == "y":
+                                            choice16 = print("You walk away in shock quivering in fear ")
+                                            choice16 = print("CHAPTER 4: LENNIE")
+                                            choice16 = input(("You meet crooks, he doesn't want you in his room, do you try and talk to him? y/n"))
+                                            if choice16 == "n":
+                                             print("you walk away and get hit by a car")
+                                            elif choice16 == "y":
+                                               choice17 = input(("You are able to talk your way into staying, do you continue to talk? y/n"))
+                                               if choice17 == "n":
+                                                print("you walk away and get hit by a car")
+                                               elif choice17 == "y":
+                                                  choice18 = input(("You get to know more about him and he tells you his struggles, the conversation end and a girl comes up to talk to you, she looks familiar, do you talk? y/n"))
+                                                  if choice18 == "n":
+                                                   print("you walk away and get hit by a car")
+                                                  elif choice18 == "y":
+                                                     choice19 = input(("You talk to her and find out shes curlys wife, do you still continue? y/n"))
+                                                     if choice19 == "n":
+                                                      print("you walk away and get hit by a car")
+                                                     elif choice19 == "y":
+                                                        choice20 = input(("You killed her what do you do? Hide her y/n?"))
+                                                        if choice20 == "n":
+                                                         print("George comes")
+                                                        elif choice20 == "y":
+                                                           choice21 = print("You hide her under hay, george comes")           
+                                                           choice21 = print("CHAPTER 5: GEORGE")
+                                                           choice21 = input(("You see lennie is all shuck up for some reason, do you investigate? y/n"))
+                                                           if choice21 == "n":
+                                                            print("You walk away and a meteor hits you")
+                                                           elif choice21 == "y":
+                                                              choice22 = input(("He killed curlys wife, what will you do? kill him or spare him?"))
+                                                              if choice22 == "spare":
+                                                               print("nope")
+                                                              elif choice22 == "kill":
+                                                                 choice23 = input(("Are you sure you want to kill him? y/n"))
+                                                                 if choice23 == "n":
+                                                                  print("You walk away to cool off but a car hits you instead")
+                                                                 elif choice23 == "y":
+                                                                    choice24 = input(("Will you lie to him? y/n"))
+                                                                 if choice24 == "n":
+                                                                  print("A meteor wipes out the entire planet")
+                                                                 elif choice24 =="y": 
+                                                                    choice25 = input(("Will you tell him you got a new job y/n"))
+                                                                    if choice25 == "n":
+                                                                     print("You lose, lennie isn't buying it")
+                                                                    elif choice25 == "y":
+                                                                       choice26 = input(("Will you talk about the rabbits y/n"))
+                                                                       if choice26 == "n":
+                                                                        print("You lose, lennie isn't buying it")
+                                                                       elif choice26 == "y":
+                                                                          choice27 = input(("Will you bring out the gun? y/n"))
+                                                                          if choice27 == "n":
+                                                                           print("You lose")
+                                                                          elif choice27 == "y":
+                                                                             choice28 = input(("Can you pull the the trigger"))
+                                                                             if choice28 == "n":
+                                                                              print("You lose")
+                                                                             elif choice28 == "y":
+                                                                                choice29 = input(("Will you pull the trigger"))
+                                                                                if choice29 == "n":
+                                                                                 print("You lose")
+                                                                                elif choice29 == "y":
+                                                                                   choice30 = input(("Do you change your mind?"))
+                                                                                   if choice30 == "y":
+                                                                                    print("You lose")
+                                                                                   elif choice30 == "n":
+                                                                                      print("You win")
 else:
-  print("You didn't order anything. Please Order Something OR GET OUT!")
-       
+   ("You died. Game Over!")
 
-print(f"Your total is {bill} ")
-
-     
-   
-    
 
 
 
